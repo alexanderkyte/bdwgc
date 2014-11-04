ffdd7c6ae143daa079b0af90d5c3795813c15497
Alexander Kyte <alexmkyte@gmail.com>
2014-11-04 08:34:17 -0500

    got low/high pc working in a portable way, I think.

--- read_types.c
+++ read_types.c
@@ -38,45 +38,93 @@ int types_finalize(void){
 }
 
 void pc_range(Dwarf_Debug dgb, Dwarf_Die fn_die, Dwarf_Addr* lowPC, Dwarf_Addr* highPC, char** dieName){
-  const char* tag_name = 0;
   Dwarf_Error err;
-  Dwarf_Half tag;
-  Dwarf_Attribute* attrs;
+
+
   Dwarf_Signed attrcount, i;
   int rc = dwarf_diename(fn_die, dieName, &err);
   
-  if (rc == DW_DLV_ERROR)
+  if (rc == DW_DLV_ERROR){
     perror("Error in dwarf_diename\n");
-  else if (rc == DW_DLV_NO_ENTRY)
+  } else if (rc == DW_DLV_NO_ENTRY){
     return;
-
-  if (dwarf_tag(fn_die, &tag, &err) != DW_DLV_OK)
-    perror("Error in dwarf_tag\n");
-  /* Only interested in subprogram DIEs here */
-
-  if (tag != DW_TAG_subprogram)
-    return;
-
-  if (dwarf_get_TAG_name(tag, &tag_name) != DW_DLV_OK)
-    perror("Error in dwarf_get_TAG_name\n");
-
-  if (dwarf_attrlist(fn_die, &attrs, &attrcount, &err) != DW_DLV_OK)
-    perror("Error in dwarf_attlist\n");
-
-  for (i = 0; i < attrcount; ++i) {
-    Dwarf_Half attrcode;
-    if (dwarf_whatattr(attrs[i], &attrcode, &err) != DW_DLV_OK)
-      perror("Error in dwarf_whatattr\n");
-    /* We only take some of the attributes for display here.
-    ** More can be picked with appropriate tag constants.
-    */
-    if (attrcode == DW_AT_low_pc)
-      dwarf_formaddr(attrs[i], lowPC, &err);
-    else if (attrcode == DW_AT_high_pc)
-      dwarf_formaddr(attrs[i], highPC, &err);
   }
 
+  if(dwarf_lowpc(fn_die, lowPC, &err) != DW_DLV_OK){
+    perror("Error in getting low pc\n");
+  }
+
+  Dwarf_Attribute highAttr;
+  if(dwarf_attr(fn_die, DW_AT_high_pc, &highAttr, &err) != DW_DLV_OK){
+    perror("Error in getting high pc attribute\n");
+  }
+  
+  Dwarf_Half highForm;
+
+  if(dwarf_whatform(highAttr, &highForm, &err) != DW_DLV_OK){
+    perror("Error in getting high pc form\n");
+  }
+
+  
+  if(highForm != DW_FORM_addr){
+
+    Dwarf_Half highEncoding;
+    
+    printf("form: %x\n", highForm);
+    dwarf_whatattr(highAttr, &highEncoding, &err);
+    printf("type: %x\n", highEncoding);
+
+    Dwarf_Signed offset = -1;
+    
+    if(dwarf_formsdata(highAttr, &offset, &err) != DW_DLV_OK){
+      perror("Error in getting high pc value.\n");
+      printf("%s\n", dwarf_errmsg(err));
+    } else if(offset < 0){
+      // Function will never have negative program count.
+      perror("Form data not read");
+    }
+
+    printf("offset: %d\n", offset);
+    
+    *highPC = *lowPC + offset;
+    
+  } else {
+    if(dwarf_highpc(fn_die, highPC, &err) != DW_DLV_OK){
+      perror("Error in getting high pc\n");
+    }
+  }
+  
+  printf("inside: %lu %lu\n", *lowPC, *highPC);
+  
+  /* for (i = 0; i < attrcount; ++i) { */
+  /*   Dwarf_Half attrcode; */
+  /*   if (dwarf_whatattr(attrs[i], &attrcode, &err) != DW_DLV_OK) */
+  /*     perror("Error in dwarf_whatattr\n"); */
+  /*   /\* We only take some of the attributes for display here. */
+  /*   ** More can be picked with appropriate tag constants. */
+  /*   *\/ */
+  /*   if (attrcode == DW_AT_low_pc){ */
+  /*     dwarf_formaddr(attrs[i], lowPC, &err); */
+  /*     printf("setting low: %lu\n", *lowPC); */
+  /*   } else if (attrcode == DW_AT_high_pc) { */
+  /*     dwarf_formexprloc(attrs[i], highPC, &err); */
+  /*     printf("setting high: %lu\n", *highPC); */
+  /*   } */
+  /* } */
+  /* printf("inside: %p %p\n", *lowPC, *highPC); */
+  
+  return;
+
 }
+
+
+/* - loop through all of them */
+/* - if function, add to function lookup */
+/* - if struct, add to struct description */
+
+
+
+/* int pointers_in_fun */
 
 void list_funcs_in_stack(Dwarf_Debug dbg, Dwarf_Addr* stack, int stackSize)
 {
@@ -101,21 +149,41 @@ void list_funcs_in_stack(Dwarf_Debug dbg, Dwarf_Addr* stack, int stackSize)
   if (dwarf_child(cu_die, &child_die, &err) == DW_DLV_ERROR)
     perror("Error getting child of CU DIE\n");
   /* Now go over all children DIEs */
-  while (1) {
-    int rc;
-    Dwarf_Addr lowPC, highPC;
-    char* dieName = calloc(sizeof(char), 50);
-    
-    pc_range(dbg, child_die, &lowPC, &highPC, &dieName);
 
-    for(int i=0; i < stackSize; i++){
-      if(stack[i] > lowPC && stack[i] < highPC){
-        printf("fn name: %s\n", dieName);
-        printf("low pc : 0x%08llx\n", lowPC);
-        printf("high pc : 0x%08llx\n", highPC);
+  for(int i=0; i < stackSize; i++){
+    printf("%lu\n", stack[i]);
+  }
+  
+  while (1) {
+
+    Dwarf_Half tag;
+    
+    if (dwarf_tag(child_die, &tag, &err) != DW_DLV_OK){
+      perror("Error in dwarf_tag\n");
+    }
+    /* Only interested in subprogram DIEs here */
+
+    if (tag == DW_TAG_subprogram){
+      Dwarf_Addr lowPC = 0;
+      Dwarf_Addr highPC = 0;
+      char* dieName = calloc(sizeof(char), 50);
+    
+      printf("\nbefore: %p %p\n", lowPC, highPC);
+      pc_range(dbg, child_die, &lowPC, &highPC, &dieName);
+      printf("after: %p %p\n\n", lowPC, highPC);
+
+      for(int i=0; i < stackSize; i++){
+        if(stack[i] > lowPC && stack[i] < highPC){
+          printf("fn name: %s\n", dieName);
+          printf("low pc : 0x%08llx\n", lowPC);
+          printf("high pc : 0x%08llx\n", highPC);
+        } else {
+          printf("=%d=%s=%d=%d=%d=\n", i, dieName, lowPC, highPC, stack[i]);
+        }
       }
     }
     
+    int rc;
     rc = dwarf_siblingof(dbg, child_die, &child_die, &err);
     if (rc == DW_DLV_ERROR)
       perror("Error getting sibling of DIE\n");
@@ -128,7 +196,7 @@ void list_funcs_in_stack(Dwarf_Debug dbg, Dwarf_Addr* stack, int stackSize)
 
 int getRoots(void){
   int frameCount = INITIAL_FRAME_SIZE;  
-  Dwarf_Addr* buffer = malloc(frameCount * sizeof(Dwarf_Addr));
+  Dwarf_Addr* buffer = calloc(frameCount, sizeof(Dwarf_Addr));
   int frames = backtrace((void **)buffer, frameCount);
   
   // Will always be equal or less.
@@ -150,6 +218,9 @@ int getRoots(void){
     if(buffer[i] == (Dwarf_Addr)0){
       frames--;
     }
+  }
+  
+  for(int i=0; i < frames; i++){
     printf("0x%08llx\n", buffer[i]);
   }
 
